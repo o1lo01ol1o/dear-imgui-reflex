f4e86a8983dab10de1afeae47bda96f73c884079
o1lo01ol1o <tim.pierson@gmail.com>
2021-01-26 09:55:31 +0000

    override dear-imgui sourrces

--- default.nix
+++ default.nix
@@ -8,11 +8,17 @@ haskellNix ? (import (import ./nix/sources.nix)."haskell.nix" { })
 
   # haskell.nix provides some arguments to be passed to nixpkgs, including some
   # patches and also the haskell.nix functionality itself as an overlay.
-, nixpkgsArgs ? haskellNix.nixpkgsArgs
-, compiler-nix-name ? "ghc884"
-}:
+, nixpkgsArgs ? haskellNix.nixpkgsArgs, compiler-nix-name ? "ghc884" }:
 let
   pkgs = import nixpkgsSrc nixpkgsArgs;
+  srcs = {
+    # Cabal doesn't fetch submodules so we need to override the sources to get the c-source submodule.
+    dear-imgui = pkgs.fetchgit {
+      url = "https://github.com/haskell-game/dear-imgui.hs";
+      rev = "2fbe257c2497ff0e2193289f33ca276baf016f93";
+      sha256 = "1n23sgmxb839n19sgvpmm3ysr9fdgq2xddagyk8hg04lpr5d6wb2";
+    };
+  };
 in pkgs.haskell-nix.project {
   inherit compiler-nix-name;
   # 'cleanGit' cleans a source directory based on the files known by git
@@ -20,4 +26,5 @@ in pkgs.haskell-nix.project {
     name = "dear-imgui-reflex";
     src = ./.;
   };
+  modules = [{ packages.dear-imgui.src = srcs.dear-imgui; }];
 }
